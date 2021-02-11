de6cb19b348cfb392217791678d235d442929f17
Salo <salomonco@gmail.com>
2021-02-11 18:49:54 -0500

    part 1 done, challenges tbd

--- books_app/__init__.py
+++ books_app/__init__.py
@@ -15,9 +15,20 @@ db = SQLAlchemy(app)
 # Authentication
 ###########################
 
-# TODO: Add authentication setup code here!
+# : Add authentication setup code here!
 
-bcrypt = None # remove me! (needed so that server will run)
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+
+from .models import User
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+bcrypt = Bcrypt(app) #(needed so that server will run)
 
 ###########################
 # Blueprints
